83a9245c6ef994bb9552e6c4247d327a683339ed
leoleeds <81908454+leoleeds@users.noreply.github.com>
2021-05-05 18:42:35 +0100

    Add files via upload

---
+++ model.py
@@ -0,0 +1,146 @@
+"""Import Libraries and Frameworks"""
+
+#import random
+#import operator
+import matplotlib.pyplot
+from agentframework import Agent
+import time
+import csv
+import tkinter
+import requests
+import bs4
+
+
+"""Initial Setup and Parameters"""
+
+start = time.time() # start time counter
+
+matplotlib.use('TkAgg') # ensure that TkInter is used
+rowlist = [] # this creates an empty row list
+environment=[] # this creates an empty environment list
+num_of_agents = 100 # this limits the number of agents
+num_of_iterations = 10
+neighbourhood = 20
+agents = [] # this creates an empty agents list
+
+
+"""Import Data from HTML"""
+
+r = requests.get('http://www.geog.leeds.ac.uk/courses/computing/practicals/python/agent-framework/part9/data.html')
+content = r.text
+soup = bs4.BeautifulSoup(content, 'html.parser')
+td_ys = soup.find_all(attrs={"class" : "y"})
+td_xs = soup.find_all(attrs={"class" : "x"})
+print(td_ys)
+print(td_xs) 
+
+
+"""Environment CSV"""
+
+f = open('in.txt', newline='') 
+reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+for row in reader:
+    rowlist = []
+    environment.append(rowlist)
+    for value in row:
+        rowlist.append(value) # fill the environment list
+        
+f.close()
+
+
+"""Verify that Environment has Imported Correctly"""
+
+#matplotlib.pyplot.imshow(environment)
+#matplotlib.pyplot.show()
+
+
+"""Make the Agents"""
+
+for i in range(num_of_agents): # this fills the list of agents
+    y = int(td_ys[i].text)
+    x = int(td_xs[i].text)
+    agents.append(Agent(environment, agents, y, x))
+    
+"""Verify that Agents are added correctly"""
+
+#print ("Initial coordinates")
+#for i in range (num_of_agents):
+#    print(agents[i].y, agents [i].x)
+    
+
+"""Define the Figure and Axes for Animation"""
+
+fig = matplotlib.pyplot.figure(figsize=(7, 7))
+ax = fig.add_axes([0, 0, 1, 1])
+
+    
+"""Generate Animated Scatter Graph of Agents"""
+
+def update(frame_number):
+    fig.clear()
+    """Call and Loop Through the Agents"""
+    for j in range (num_of_iterations):
+        for i in range (num_of_agents):
+            agents[i].move()
+            agents[i].eat()
+            agents[i].share_with_neighbours(neighbourhood)
+    """Generate Scatter Graph of Agents"""
+    matplotlib.pyplot.xlim(0, 99)
+    matplotlib.pyplot.ylim(0, 99)
+    matplotlib.pyplot.imshow(environment)
+    for i in range(num_of_agents):
+        matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
+#    matplotlib.pyplot.show()
+
+#animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, frames=num_of_iterations, repeat=False)
+
+
+"""Run Model"""
+
+def run():
+    animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, frames=num_of_iterations, repeat=False)
+    canvas.draw()
+    
+    
+"""Build the Canvas"""
+
+root = tkinter.Tk()
+root.wm_title("Model")
+canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
+canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+
+"""Create Menu"""
+
+menu_bar = tkinter.Menu(root)
+root.config(menu=menu_bar)
+model_menu = tkinter.Menu(menu_bar)
+menu_bar.add_cascade(label="Model", menu=model_menu)
+model_menu.add_command(label="Run model", command=run)
+    
+    
+"""Verify Final Agents"""
+#print("Final coordinates")
+#for i in range (num_of_agents):
+#    print(agents[i].y, agents[i].x)
+
+
+"""Output to .CSV"""
+
+f2 = open('output.csv','w',newline='')
+writer = csv.writer(f2, delimiter = ' ')
+
+for row in environment:
+    writer.writerow(row)
+f2.close()
+
+
+"""Check Timing"""
+
+#end = time.time() # end time counter
+#print(end - start) # processing time in seconds
+
+
+"""Await User Input"""
+
+tkinter.mainloop() 
